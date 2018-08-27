7720dc1788dd78f8dd0cdfa08fd82a916570b667
rhys5690 <rhys5690@gmail.com>
2018-08-28 07:48:54 +1000

    updated global state

---
+++ src/Posts/EditMode.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ApolloConsumer } from 'react-apollo';
+
+export default class EditMode extends Component {
+  render() {
+    const { isEditMode } = this.props;
+    return (
+      <div>
+        <ApolloConsumer>{(client) => <button
+          onClick={() => {
+            client.writeData({ data: {isEditMode: !isEditMode} })
+          }}
+        >Toggle Edit Mode</button>}</ApolloConsumer>
+      </div>
+    );
+  }
+}

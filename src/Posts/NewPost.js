af70bf29a443a694016c7d78edf77cc662f4e1f0
rhys5690 <rhys5690@gmail.com>
2018-08-27 13:58:01 +1000

    created reuseable form

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 import PostForm from './PostForm';
 
 export default class NewPost extends Component {
@@ -6,8 +8,22 @@ export default class NewPost extends Component {
     return (
       <div>
         <h1>New Post</h1>
-        <PostForm />
+        <Mutation
+          mutation={NEW_POST}
+        >
+          {(createPost) => <PostForm onSubmit={createPost}/>}
+        </Mutation>
       </div>
     );
   }
 }
+
+const NEW_POST = gql`
+  mutation createPost($title: String!, $body: String!) {
+    createPost(data: { status: PUBLISHED, title: $title, body: $body }) {
+      title
+      body
+      id
+    }
+  }
+`;
--- src/Posts/PostForm.js
+++ src/Posts/PostForm.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import gql from 'graphql-tag';
-import { Mutation } from 'react-apollo';
 
 export default class PostForm extends Component {
   state = {
@@ -16,53 +14,43 @@ export default class PostForm extends Component {
     });
   };
   render() {
+    const { onSubmit } = this.props;
     const { title, body } = this.state;
     return (
-      <Mutation
-        mutation={NEW_POST}
-        variables={{
-          title,
-          body
-        }}
-      >
-        {createPost => (
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            createPost().then(() => {
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit({
+            variables: {
+              title,
+              body
+            }
+          })
+            .then(() => {
               this.setState({
                 title: '',
                 body: ''
-              })
-            }).catch(err => console.log(err))
-          }}>
-            <input
-              name="title"
-              type="text"
-              placeholder="title"
-              onChange={this.handleInput}
-              value={title}
-            />
-            <textarea
-              name="body"
-              type="text"
-              placeholder="body"
-              onChange={this.handleInput}
-              value={body}
-            />
-            <button>Submit</button>
-          </form>
-        )}
-      </Mutation>
+              });
+            })
+            .catch((err) => console.log(err));
+        }}
+      >
+        <input
+          name="title"
+          type="text"
+          placeholder="title"
+          onChange={this.handleInput}
+          value={title}
+        />
+        <textarea
+          name="body"
+          type="text"
+          placeholder="body"
+          onChange={this.handleInput}
+          value={body}
+        />
+        <button>Submit</button>
+      </form>
     );
   }
 }
-
-const NEW_POST = gql`
-  mutation createPost($title: String!, $body: String!) {
-    createPost(data: { status: PUBLISHED, title: $title, body: $body }) {
-      title
-      body
-      id
-    }
-  }
-`;
